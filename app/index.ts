32e54c7cce368ba483967ce430e6bd09ff1dd7d6
Howard <hw@yuanhau.com>
2025-05-26 12:38:18 +0800

    Add createdatabase endpoint && Delete public folder as docker can't use it.

--- app/index.ts
+++ app/index.ts
@@ -11,11 +11,6 @@ import { exportNewView2 } from "./components/exportView2";
 import { fcjaauwi } from "./components/savs";
 import { exportChangeType2, gfa } from "./components/exportChangeType2";
 
-const webSocketJs = readFileSync(
-  join(process.cwd(), "public", "websocket.js"),
-  "utf8",
-);
-
 const errorpage = readFileSync(
   join(process.cwd(), "app", "errorpage.html"),
   "utf8",
@@ -111,6 +106,50 @@ const routes: Record<string, RouteHandler> = {
         { status: 403 },
       );
     }
+  },
+  "/logger/createdbase": async () => {
+    const create1 = await sql`
+CREATE TABLE IF NOT EXISTS logger (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    cwa_type VARCHAR(50),
+    cwa_location VARCHAR(100),
+    cwa_temp DECIMAL(5,2),
+    cwa_hum DECIMAL(5,2),
+    cwa_daily_high DECIMAL(5,2),
+    cwa_daily_low DECIMAL(5,2),
+    local_temp DECIMAL(5,2),
+    local_hum DECIMAL(5,2),
+    local_gps_lat VARCHAR(20),
+    local_gps_long VARCHAR(20),
+    local_time TIMESTAMPTZ,
+    local_jistatus BOOLEAN,
+    local_light BOOLEAN,
+    local_detect JSONB
+);
+`;
+
+    const create2 = await sql`
+CREATE TABLE IF NOT EXISTS detect (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    item text not null,
+    imageURL text not null,
+    detected_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+)
+`;
+
+    const create3 = await sql`
+create table if not exists jistatus (
+    id SERIAL PRIMARY KEY,
+    status boolean not null
+)
+`;
+    return new Response("Database created successfully", {
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
   },
   "/logger/ledstatus": async (req: Request) => {
     return new Response(await exportChangeType2(), {
